b99debedd30ac084a38b80951e19e082439e77f9
Leslie <mukondiwaleslie@gmail.com>
2023-05-06 21:10:29 +0200

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,11 +7,18 @@
  **/
 
 void print_binary(unsigned long int n) {
-    unsigned long int i = 1UL << (sizeof(unsigned long int) * 8 - 1);
-    while (i > 0) {
-        putchar((n & i) ? '1' : '0');
-        i >>= 1;
+    int bit_pos = sizeof(n) * 8 - 1; // position of the leftmost bit
+    while ((n >> bit_pos) == 0 && bit_pos > 0) {
+        bit_pos--; // skip leading zeros
     }
+    while (bit_pos >= 0) {
+        int bit = (n >> bit_pos) & 1; // get the current bit
+        printf("%d", bit);
+        bit_pos--; // move to the next bit
+    }
+    printf("\n");
 }
+
+
 
 
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,13 +8,8 @@
  * returns: the value of a bit at index (index) or -1 if an error occured
  *
  **/
-
-int get_bit(unsigned long int n, unsigned int index) {
-    if (index >= sizeof(unsigned long int) * 8) {
-        return -1;
-    }
-
-    unsigned long int mask = 1UL << index;
-    return (n & mask) ? 1 : 0;
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((index > (8 * sizeof(n) - 1)) ? -1 : (int)(n >> index) & 1);
 }
 
